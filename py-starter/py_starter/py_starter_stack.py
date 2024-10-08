7372204cf8f87cc73462643da8deec16f981869d
Sergio Trujillo <sergio.trujillo@mxmart.mx>
2024-10-08 12:37:48 -0600

    creation of lambda conected with api gateway

--- py-starter/py_starter/py_starter_stack.py
+++ py-starter/py_starter/py_starter_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway,
 )
 from constructs import Construct
 
@@ -10,10 +10,26 @@ class PyStarterStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Define the Lambda function
+        lambda_function = _lambda.Function(
+            self, "MyLambdaFunction",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler='lambda_function.handler',
+            code=_lambda.Code.from_asset('resources/lambdas/lambda_function')
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PyStarterQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the API Gateway REST API
+        api = apigateway.RestApi(
+            self, 'MyApiGateway',
+            rest_api_name='My Service',
+            description='This service serves my Lambda function.'
+        )
+
+        # Create a Lambda integration
+        lambda_integration = apigateway.LambdaIntegration(
+            lambda_function,
+            request_templates={"application/json": '{ "statusCode": "200" }'}
+        )
+
+        # Add a method to the API Gateway resource
+        api.root.add_method('GET', lambda_integration)
